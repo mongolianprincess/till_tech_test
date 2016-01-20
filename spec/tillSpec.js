d22f1eb5ee1c657e7457b400dec7f2fc5c8f53bf
mongolianprincess <ayc.yang@gmail.com>
2016-01-20 13:14:13 +0000

    Passed input multiple order test and written calculate sum test

--- spec/tillSpec.js
+++ spec/tillSpec.js
@@ -8,19 +8,19 @@ describe ('till',function(){
     till = new Till();
   })
 
-  it('has a shop name',function(){
+  it('has a shop name',function() {
     expect(till.shopName).toEqual("The Coffee Connection");
   });
 
-  it('has an address',function(){
+  it('has an address',function() {
     expect(till.address).toEqual("123 Lakeside Way");
   });
 
-  it('has a price list',function(){
+  it('has a price list',function() {
     expect(till.priceList).toEqual(menu);
   });
 
-  it('has an order', function(){
+  it('has an order', function() {
     expect(till.order).toBeDefined();
   });
 
@@ -29,8 +29,20 @@ describe ('till',function(){
     expect(till.orderName).toEqual(["Amy"]);
   });
 
-  it('takes an order of item and quantity',function(){
+  it('takes an order of item and quantity',function() {
     till.inputOrder("Tea", 2);
     expect(till.order).toEqual([{Tea:2}]);
+  });
+
+  it('takes multiple order of items', function() {
+    till.inputOrder("Tea", 2);
+    till.inputOrder("Burger", 5);
+    expect(till.order).toEqual([{Tea:2}, {Burger:5}])
+  });
+
+  it('calculates the sum total of items',function() {
+    till.inputOrder("Tea",2);
+    till.calculateSum();
+    expect(till.sumTotal).toEqual(4);
   });
 })
